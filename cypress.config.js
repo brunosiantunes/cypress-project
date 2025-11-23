2cf2da14d42485c9940f4052d6e73f79ca4a8fc5
Bruno Antunes <brunosiantunes@gmail.com>
2025-11-23 11:10:09 -0300

    Criando o projeto

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,7 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+   projectId: "t9g5v3",
   e2e: {
     reporter: 'cypress-mochawesome-reporter',
     reporterOptions: {
